182ce6105888b0bb5a05c11e72ab2438af685eae
kirill <kikochnev@yandex.ru>
2015-03-13 17:18:04 +0300

    two-dimensional

--- Matlab/Kalman.m
+++ Matlab/Kalman.m
@@ -14,10 +14,8 @@
 %
 % $$x_{k+1} = Fx_k+w_k$$
 %% Intitialization
-x = zeros(3,100);
-z = zeros(3,100);
-%x = zeros(1, 100);
-%z = zeros(1, 100);
+x = zeros(6,100);
+z = zeros(6,100);
 sigma = 1;
 sigma0 = 1;
 T = 1;
@@ -27,23 +25,27 @@ for i=2:100
     k(i) = k(i-1)+T;
 end
 
-F = [1 T T*T/2; 0 1 T; 0 0 1];
-P = [sigma*sigma 3*sigma*sigma/(2*T) sigma*sigma/(T*T); 3*sigma*sigma/(2*T) 13*sigma*sigma/(2*T*T) 6*sigma*sigma/(T*T*T); sigma*sigma/(T*T) 6*sigma*sigma/(T*T*T) 6*sigma*sigma/(T*T*T*T)];
-H = [1 0 0];
-R = sigma;
-Q = zeros(3);
-q = 0;
-I = [1 0 0; 0 1 0; 0 0 1];
-K = zeros(3,1);
+F = [1 T T*T/2 0 0 0; 0 1 T 0 0 0; 0 0 1 0 0 0; 0 0 0 1 T T*T/2; 0 0 0 0 1 T; 0 0 1 0 0 1;];
+P = [sigma*sigma        3*sigma*sigma/(2*T)    sigma*sigma/(T*T)       0                   0                      0;
+    3*sigma*sigma/(2*T) 13*sigma*sigma/(2*T*T) 6*sigma*sigma/(T*T*T)   0                   0                      0;
+    sigma*sigma/(T*T)   6*sigma*sigma/(T*T*T)  6*sigma*sigma/(T*T*T*T) 0                   0                      0;
+    0                   0                      0                       sigma*sigma         3*sigma*sigma/(2*T )   sigma*sigma/(T*T);
+    0                   0                      0                       3*sigma*sigma/(2*T) 13*sigma*sigma/(2*T*T) 6*sigma*sigma/(T*T*T);
+    0                   0                      0                       sigma*sigma/(T*T)   6*sigma*sigma/(T*T*T)  6*sigma*sigma/(T*T*T*T)];
+H = [1 0 0 0 0 0; 0 0 0 1 0 0];
+R = [sigma 0; 0 sigma];
+Q = zeros(6);
+q = [0 0];
+I = [1 0 0 0 0 0; 0 1 0 0 0 0; 0 0 1 0 0 0; 0 0 0 1 0 0; 0 0 0 0 1 0; 0 0 0 0 0 1];
+K = zeros(6,2);
 
 %% Test
-z(:,1) = [0 0 2];
-z(:,2) = [1 2 2];
-z(:,3) = [4 4 2];
+z(:,1) = [0; 0; 2; 0; 0; 2];
+z(:,2) = [1; 2; 2; 1; 2; 2];
+z(:,3) = [4; 4; 2; 4; 4; 2];
 
-x_k = zeros(3, 1);
-x_k_1 = zeros(3, 1);
-
+x_k = zeros(6, 1);
+x_k_1 = zeros(6, 1);
 for i=1:100
     if i==1
         x_k = z(:,1);
@@ -56,8 +58,7 @@ for i=1:100
         x(:,3) = x_k_1;
     else
         %prediction
-        %z(:,i) = F*z(:,i-1);
-        [t,Y] = ode45(@Kalman_moving, [0 i], [0 0 2]);
+        [t, Y] = ode45(@Kalman_moving, [0 i], [0 0 2 0 0 2]);
         z(:,i) = Y(end,:);
         x_k = F*x_k_1;
         x(:,i) = F*x_k;
@@ -66,15 +67,14 @@ for i=1:100
         
         %correction
         K = P*H'/(H*P*H'+R);
-        
-        x_k = x_k + K*(z(1,i) - H*x_k);
+
+        x_k = x_k + K*([z(1,i); z(4,i)] - H*x_k);
         P = (I-K*H)*P;       
         
         i
         n=i;
-        P
-        %P = vpa(P, 10)
-        
+        P = vpa(P, 10)
+       
         k11 = 3*(3*n*n - 3*n + 2)/(n*(n+1)*(n+2));
         k12 = - 18*(2*n-1)/(n*(n+1)*(n+2));
         k13 = 60/(n*(n+1)*(n+2));
@@ -82,29 +82,50 @@ for i=1:100
         k23 = 360/(n*(n*n-4)*(n+1));
         k33 = 720/(n*(n*n-4)*(n*n-1));
         
-        Pnew = [k11 k12 k13; k12 k22 k23; k13 k23 k33]
-        %Pnew = vpa(Pnew, 10)
+        Pnew = [k11 k12 k13; k12 k22 k23; k13 k23 k33];
+        Pnew = vpa(Pnew, 10)
         x_k_1 = x_k;
     end    
 end
 
-subplot(1,3,1);
+subplot(2,3,1);
 plot(k, x(1,:), '-', k, z(1,:), 'r-.');
-title('Coordinate Filter');
+title('x Coordinate Filter');
 xlabel('t, c');
 ylabel('coordinate, m');
 legend('meassurement', 'filter');
 
-subplot(1,3,2);
+subplot(2,3,2);
 plot(k, x(2,:), '-', k, z(2,:), 'r-.');
-title('Velocity Filter');
+title('x Velocity Filter');
 xlabel('t, c');
 ylabel('velocity, m/c');
 legend('meassurement', 'filter');
 
-subplot(1,3,3);
+subplot(2,3,3);
 plot(k, x(3,:), '-', k, z(3,:), 'r-.');
-title('Acceleration Filter');
+title('x Acceleration Filter');
+xlabel('t, c');
+ylabel('acceleration, m/c*c');
+legend('meassurement', 'filter');
+
+subplot(2,3,4);
+plot(k, x(4,:), '-', k, z(4,:), 'r-.');
+title('y Coordinate Filter');
+xlabel('t, c');
+ylabel('coordinate, m');
+legend('meassurement', 'filter');
+
+subplot(2,3,5);
+plot(k, x(5,:), '-', k, z(5,:), 'r-.');
+title('y Velocity Filter');
+xlabel('t, c');
+ylabel('velocity, m/c');
+legend('meassurement', 'filter');
+
+subplot(2,3,6);
+plot(k, x(6,:), '-', k, z(6,:), 'r-.');
+title('y Acceleration Filter');
 xlabel('t, c');
 ylabel('acceleration, m/c*c');
 legend('meassurement', 'filter');
