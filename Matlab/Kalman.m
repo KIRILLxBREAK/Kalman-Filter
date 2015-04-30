ea61c1748538c8e6a02d2fc4671385f1a0370d99
Kirill <kikochnev@yandex.ru>
2015-04-30 17:07:38 +0300

    new n=1

--- Matlab/Kalman.m
+++ Matlab/Kalman.m
@@ -5,7 +5,7 @@
 % $$z_k = x_k + h_k$$
 %
 % *z[2]* - observation data
-%   
+%
 % *w[2]* - measuring error
 %
 % *x[2]* - our hidden variable, which we want to filter;
@@ -15,175 +15,99 @@
 % $$x_{k+1} = Fx_k+w_k$$
 %% Intitialization
 N = 100;
-x = zeros(6,N);
-%xF = zeros(6,N);
-z = zeros(6,N);
-
-x_k = zeros(6,1);
-%x_kF = zeros(6,1);
-x_k_1 = zeros(6,1);
-%x_k_1F = zeros(6,1);
-
-sigma = 0.1;
-sigmaF = 0.1;
+sigma = 0;
+sigmaR = 1;
 T = 1;
-%% moving
-tspan = 1:T:N*T; 
-[t, Y] = ode45(@Kalman_moving, tspan, [0 0 2 0 0 2]);
-%plot(t, Y(:,1));
-[fi, Rad] = cart2pol(Y(:,1), Y(:,4));
 
-F = [1 T T*T/2 0 0    0;
-     0 1   T   0 0    0;
-     0 0   1   0 0    0;
-     0 0   0   1 T  T*T/2;
-     0 0   0   0 1    T;
-     0 0   0   0 0    1;];
-% sigmaX  = (sigma*cos(fi(3)))^2     + (Rad(3)*sigmaF*sin(fi(3)))^2;
-% sigmaY  = (sigma*sin(fi(3)))^2     + (Rad(3)*sigmaF*cos(fi(3)))^2;
-% sigmaXY = (sin(2*fi(3))*sigma^2)/2 - (sin(2*fi(3))*(Rad(3)*sigmaF)^2)/2;
-% 
-% PF = [sigmaX         3*sigmaX/(2*T)     sigmaX/(T*T)       sigmaXY         3*sigmaXY/(2*T)    sigmaXY/(T*T);
-%      3*sigmaX/(2*T)  13*sigmaX/(2*T*T)  6*sigmaX/(T*T*T)   3*sigmaXY/(2*T) 13*sigmaXY/(2*T*T) 6*sigmaXY/(T*T*T);
-%      sigmaX/(T*T)    6*sigmaX/(T*T*T)   6*sigmaX/(T*T*T*T) sigmaX/(T*T)    6*sigmaX/(T*T*T)   6*sigmaX/(T*T*T*T);
-%      sigmaXY         3*sigmaXY/(2*T)    sigmaXY/(T*T)      sigmaY          3*sigmaY/(2*T )    sigmaY/(T*T);
-%      3*sigmaXY/(2*T) 13*sigmaXY/(2*T*T) 6*sigmaXY/(T*T*T)  3*sigmaY/(2*T)  13*sigmaY/(2*T*T)  6*sigmaY/(T*T*T);
-%      sigmaX/(T*T)    6*sigmaX/(T*T*T)   6*sigmaX/(T*T*T*T) sigmaY/(T*T)    6*sigmaY/(T*T*T)   6*sigmaY/(T*T*T*T)];
+F = [1 T; 0 1];
+P = [sigmaR sigmaR/T; sigmaR/T 2*sigmaR/(T*T)];
+H = [1 0];
+R = sigma;
+Q = zeros(2);
+I = [1 0; 0 1];
 
-P = [sigma^2        3*sigma^2/(2*T)    sigma^2/(T*T)       0               0                  0;
-    3*sigma^2/(2*T) 13*sigma^2/(2*T*T) 6*sigma^2/(T*T*T)   0               0                  0;
-    sigma^2/(T*T)   6*sigma^2/(T*T*T)  6*sigma^2/(T*T*T*T) 0               0                  0;
-    0               0                  0                   sigma^2         3*sigma^2/(2*T )   sigma^2/(T*T);
-    0               0                  0                   3*sigma^2/(2*T) 13*sigma^2/(2*T*T) 6*sigma^2/(T*T*T);
-    0               0                  0                   sigma^2/(T*T)   6*sigma^2/(T*T*T)  6*sigma^2/(T*T*T*T)];
+x = zeros(2,N);
+xF = zeros(2,N);
+z = zeros(2,N);
+Krv = 0;
+Kvv = 0;
+pfk = zeros(2);
 
-H = [1 0 0 0 0 0;
-     0 0 0 1 0 0];
-% R = [sigma^2    0;
-%       0          sigma^2];
-R = [0 0; 0 0]; 
-% RF = [sigmaX    sigmaXY;
-%       sigmaXY   sigmaY];
-Q = zeros(6);
-q = [0 0];
-I = [1 0 0 0 0 0;
-     0 1 0 0 0 0;
-     0 0 1 0 0 0;
-     0 0 0 1 0 0;
-     0 0 0 0 1 0;
-     0 0 0 0 0 1];
-K = zeros(6,2);
-%KF = zeros(6,2);
+K = zeros(2, 1);
 
-%% filtering
-format long g;
 
+k = zeros(1,N);
+for i=2:N
+    k(i) = k(i-1)+T;
+end
+%% Test
+z(:,1) = [1 8000];
+z(:,2) = [2 1];
 for i=1:N
     if i==1
-        %measurement
-        [z(1,i), z(4,i)] = pol2cart(fi(i),Rad(i));
-        
-        x_k = z(:,i);
-        x(:,i) = z(:,i);
-%         x_kF = z(:,i);
-%         xF(:,i) = z(:,i);
+        x(:,1) = z(:,1);
+        xF(:,1) = z(:,1);
     elseif i==2
-        %measurement
-        [z(1,i), z(4,i)] = pol2cart(fi(i),Rad(i));
-        
-        x_k_1 = z(:,i);
-        x(:,i) = z(:,i);
-%         x_k_1F = z(:,i);
-%         xF(:,i) = z(:,i);
-    elseif i==3
-        [z(1,i), z(4,i)] = pol2cart(fi(i),Rad(i));
-        z(3,i) = (z(1,i) - 2*z(1,i-1) + z(1,i-2))/(T*T);
-        z(2,i) = (z(1,i) - z(1,i-2))/(2*T) + z(3,i)*T;
-        z(6,i) = (z(4,i) - 2*z(4,i-1) + z(4,i-2))/(T*T);
-        z(5,i) = (z(4,i) - z(4,i-2))/(2*T) + z(6,i)*T;
-        
-        x_k_1 = z(:,i);
-        x(:,i) = z(:,i);
-%         x_k_1F = z(:,i);
-%         xF(:,i) = z(:,i);
-    else    
-        %measurement      
-        [z(1,i), z(4,i)] = pol2cart(fi(i),Rad(i));
-        z(3,i) = (z(1,i) - 2*z(1,i-1) + z(1,i-2))/(T*T);
-        z(2,i) = (z(1,i) - z(1,i-2))/(2*T) + z(3,i)*T;
-        z(6,i) = (z(4,i) - 2*z(4,i-1) + z(4,i-2))/(T*T);
-        z(5,i) = (z(4,i) - z(4,i-2))/(2*T) + z(6,i)*T;
-        
+        x(1,2)=z(1,2);
+        x(2,2)=(x(1,2)-x(1,1))/T;
+        xF(1,2)=z(1,2);
+        xF(2,2)=(xF(1,2)-xF(1,1))/T;
         i
-        %prediction 
-%         x_k = F*x_k_1;
-%         x(:,i) = x_k;
-%         x_kF = F*x_k_1F;
-%         xF(:,i) = x_kF;
+        Krv = sigmaR/T;
+        Kvv = 2*sigmaR/T*T;
+        pfk = [sigmaR Krv; Krv Kvv]
+    else
+        %prediction
+        P = F*P*F' + Q; 
+        sigmaR = (sigmaR + 2*Krv*T + Kvv*T*T);
+        Krv = Krv + Kvv*T;  
+        pfk = [sigmaR Krv; Krv Kvv] + Q;
         
-        P = F*P*F'+Q;
-%         PF = F*PF*F'+Q;
-        
+        z(:,i) = F*z(:,i-1);
+        x(:,i) = F*x(:,i-1);
+        xF(:,i) = F*xF(:,i-1);
+
         %correction
-        K = P*H'/(H*P*H');%+R)
-%         KF = PF*H'/(H*PF*H'+RF);
+        i
+        if R == zeros(2)
+            %G->H'
+            P = 0
+            pfk = 0
+            
+            %x->H'*z
+            x(1,i) = z(1,i);
+            xF(1,i) = z(1,i);
+        elseif P == zeros(2)
+            %G->0
+            P
+            pfk
+        else
+            %Kalman gain(blending factor)
+            G = P*H'/(H*P*H'+R)
+            W = 1/(sigmaR+R);
+            Wv = Krv/(sigmaR+R);
         
-%         x_kF = x_kF + KF*([z(1,i); z(4,i)] - H*x_kF)
-%         x_k = x_k + K*([z(1,i); z(4,i)] - H*x_k)
-        
-        P = (I-K*H)*P
-%         PF = (I-KF*H)*PF;
-        
-%         disp('KF=');
-%         disp(num2str(KF,'% f'));
-%         disp('K=');
-%         disp(num2str(K,'% f'));
-%         PF = vpa(PF, 10)
-%         P = vpa(P, 10)
-%         disp('PF=');
-%         disp(num2str(PF,'% f'));
-%         disp('P=');
-%         disp(num2str(P,'% f'));
-    end    
+            P = (I-G*H)*P
+            sigmaR = W*sigmaR*R;
+            Kvv = Kvv - Wv*Krv;
+            Krv = W*R*Krv;        
+            pfk = [sigmaR Krv; Krv Kvv]
+%         K = [W*sigmaR; Wv]
+%         pfk = (I-K*H)*pfk         
+%         sigmaR = pfk(1,1);
+%         Krv = pfk(1,2);
+%         Kvv = pfk(2,2);
+
+        x(:,i) = x(:,i) + G*(z(1,i) - H*x(:,i));
+        xF(:,i) = xF(:,i) + K*(z(1,i)-H*xF(:,i));
+        end
+    end
 end
 
-% subplot(2,3,1);
-% plot(k, x(1,:), '-', k, z(1,:), 'r-.');
-% title('x Coordinate Filter');
-% xlabel('t, c');
-% ylabel('coordinate, m');
-% legend('meassurement', 'filter');
-% 
-% subplot(2,3,2);
-% plot(k, x(2,:), '-', k, xF(2,:), 'r-.');
-% title('x Velocity Filter');
-% xlabel('t, c');
-% ylabel('velocity, m/c');
-% legend('meassurement', 'filter');
-% 
-% subplot(2,3,3);
-% plot(k, x(3,:), '-', k, xF(3,:), 'r-.');
-% title('x Acceleration Filter');
-% xlabel('t, c');
-% ylabel('acceleration, m/c*c');
-% legend('meassurement', 'filter');
-% 
-% subplot(2,3,4);
-% plot(k, x(4,:), '-', k, xF(4,:), 'r-.');
-% title('y Coordinate Filter');
-% xlabel('t, c');
-% ylabel('coordinate, m');
-% legend('meassurement', 'filter');
-% 
-% subplot(2,3,5);
-% plot(k, x(5,:), '-', k, xF(5,:), 'r-.');
-% title('y Velocity Filter');
-% xlabel('t, c');
-% ylabel('velocity, m/c');
-% legend('meassurement', 'filter');
-% 
-% subplot(2,3,6);
-% plot(k, x(6,:), '-', k, xF(6,:), 'r-.');
-% title('y Acceleration Filter');
-% xlabel('t, c');
-% ylabel('acceleration, m/c*c');
+plot(k, z(1,:), 'r')
+title('Kalman filter');
+xlabel('t, c');
+ylabel('coordinate, m');
+hold on;
+plot(k, x(1,:), 'y')
+legend('meassurement', 'filter');
