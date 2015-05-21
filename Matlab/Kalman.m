da3ed23153cc6b6ca4639f6e58102c2088f0a159
Kirill <kikochnev@yandex.ru>
2015-05-21 13:29:59 +0300

    with acceleration

--- Matlab/Kalman.m
+++ Matlab/Kalman.m
@@ -15,60 +15,87 @@
 % $$x_{k+1} = Fx_k+w_k$$
 %% Intitialization
 N = 100;
-sigma = 0;
+sigma = 1;
 sigmaR = 1;
 T = 1;
 
-F = [1 T; 0 1];
-P = [sigmaR sigmaR/T; sigmaR/T 2*sigmaR/(T*T)];
-H = [1 0];
+F = [1 T T*T/2; 0 1 T; 0 0 1];
+P = [sigmaR        3*sigmaR/(2*T)       sigmaR/(T*T);
+    3*sigmaR/(2*T) 13*sigmaR/(2*T*T)    6*sigmaR/(T*T*T);
+    sigmaR/(T*T)   6*sigmaR/(T*T*T)     6*sigmaR/(T*T*T*T)];
+H = [1 0 0];
 R = sigma;
-Q = zeros(2);
-I = [1 0; 0 1];
+Q = zeros(3);
+I = [1 0 0; 0 1 0; 0 0 1];
 
-x = zeros(2,N);
-xF = zeros(2,N);
-z = zeros(2,N);
+x = zeros(3,N);
+xF = zeros(3,N);
+z = zeros(3,N);
 Krv = 0;
 Kvv = 0;
-pfk = zeros(2);
+Kra = 0;
+Kva = 0;
+Kaa = 0;
+pfk = zeros(3);
 
-K = zeros(2, 1);
-
+K = zeros(3, 1);
+Gz = zeros(3, N);
 
 k = zeros(1,N);
 for i=2:N
     k(i) = k(i-1)+T;
 end
 %% Test
-z(:,1) = [1 8000];
-z(:,2) = [2 1];
+z(:,1) = [0 0 2];
+z(:,2) = [1 2 2];
+z(:,3) = [4 4 2];
+
+x_k = zeros(3, 1);
+x_k_1 = zeros(3, 1);
+
 for i=1:N
     if i==1
-        x(:,1) = z(:,1);
-        xF(:,1) = z(:,1);
+        x(1,i) = z(1,i);
+        xF(1,i) = z(1,i);
     elseif i==2
-        x(1,2)=z(1,2);
-        x(2,2)=(x(1,2)-x(1,1))/T;
-        xF(1,2)=z(1,2);
-        xF(2,2)=(xF(1,2)-xF(1,1))/T;
-        i
-        Krv = sigmaR/T;
-        Kvv = 2*sigmaR/T*T;
-        pfk = [sigmaR Krv; Krv Kvv]
+        x(1,i) = z(1,i);
+        xF(1,i) = z(1,i);
+    elseif i==3
+        x(1,i) = z(1,i);
+        xF(1,i) = z(1,i);
+        %завязка
+        x(3,i) = (x(1,i) - 2*x(1,i-1) + x(1,i-2))/(T*T);
+        x(2,i) = (x(1,i) - x(1,i-2))/(2*T) + x(3,i)*T;
+        %x(6,i) = (x(4,i) - 2*x(4,i-1) + x(4,i-2))/(T*T);
+        %x(5,i) = (x(4,i) - x(4,i-2))/(2*T) + x(6,i)*T;
+        
+        xF(3,i) = (xF(1,i) - 2*xF(1,i-1) + xF(1,i-2))/(T*T);
+        xF(2,i) = (xF(1,i) - xF(1,i-2))/(2*T) + xF(3,i)*T;
+        
+        Krv = 3*sigmaR/(2*T);
+        Kra = sigmaR/(T*T);
+        Kvv = 13*sigmaR/(2*T*T);
+        Kva = 6*sigmaR/(T*T*T);
+        Kaa = 6*sigmaR/(T*T*T*T);
     else
         %prediction
-        P = F*P*F' + Q; 
-        sigmaR = (sigmaR + 2*Krv*T + Kvv*T*T);
-        Krv = Krv + Kvv*T;  
-        pfk = [sigmaR Krv; Krv Kvv] + Q;
+        P = F*P*F'+Q;
+        sigmaR = sigmaR + 2*T*Krv + T*T*(Kra+Kvv) + T*T*T*Kva + T*T*T*T*Kaa/4;
+        Krv = Krv + T*(Kra+Kvv) + 3*T*T*Kva/2 + T*T*T*Kaa/2;
+        Kra = Kra + T*Kva + T*T*Kaa/2;
+        Kvv = Kvv + 2*T*Kva + T*T*Kaa;
+        Kva = Kva + T*Kaa;
+        pfk = [sigmaR Krv Kra; Krv Kvv Kva; Kra Kva Kaa] + Q;
         
         z(:,i) = F*z(:,i-1);
+        %[t,Y] = ode45(@Kalman_moving, [0 i], [0 0 2]);
+        %z(:,i) = Y(end,:);
+        %x_k = F*x_k_1;
         x(:,i) = F*x(:,i-1);
-        xF(:,i) = F*xF(:,i-1);
-
-        %correction
+        xF(:,i) = F*xF(:,i-1); 
+        
         i
+        %correction
         if R == zeros(2)
             %G->H'
             P = 0
@@ -77,37 +104,62 @@ for i=1:N
             %x->H'*z
             x(1,i) = z(1,i);
             xF(1,i) = z(1,i);
-        elseif P == zeros(2)
+        elseif P == zeros(3)
             %G->0
             P
             pfk
         else
             %Kalman gain(blending factor)
-            G = P*H'/(H*P*H'+R)
+            G = P*H'/(H*P*H'+R);
             W = 1/(sigmaR+R);
             Wv = Krv/(sigmaR+R);
-        
-            P = (I-G*H)*P
+            K = [W*sigmaR; W*Krv; W*Kra];
+            
+                       
+            P = (I-K*H)*P
             sigmaR = W*sigmaR*R;
-            Kvv = Kvv - Wv*Krv;
-            Krv = W*R*Krv;        
-            pfk = [sigmaR Krv; Krv Kvv]
-%         K = [W*sigmaR; Wv]
-%         pfk = (I-K*H)*pfk         
-%         sigmaR = pfk(1,1);
-%         Krv = pfk(1,2);
-%         Kvv = pfk(2,2);
-
-        x(:,i) = x(:,i) + G*(z(1,i) - H*x(:,i));
-        xF(:,i) = xF(:,i) + K*(z(1,i)-H*xF(:,i));
+            Kaa = Kaa - W*Kra*Kra;
+            Kvv = Kvv  - Wv*Krv;
+            Kva = Kva - W*Krv*Kra;
+            Krv = W*R*Krv;
+            Kra = W*R*Kra;
+            pfk = [sigmaR Krv Kra; Krv Kvv Kva; Kra Kva Kaa]
+               
+            n=i;  
+            k11 = 3*(3*n*n - 3*n + 2)/(n*(n+1)*(n+2));
+            k12 = - 18*(2*n-1)/(n*(n+1)*(n+2));
+            k13 = 60/(n*(n+1)*(n+2));
+            k22 = (12*(2*n-1)*(8*n-11))/(n*(n*n-4)*(n*n-1));
+            k23 = 360/(n*(n*n-4)*(n+1));
+            k33 = 720/(n*(n*n-4)*(n*n-1));
+        
+            Pnew = [k11 k12 k13; k12 k22 k23; k13 k23 k33]
+%         %Pnew = vpa(Pnew, 10)
+%         x_k_1 = x_k;
+%x_k = x_k + K*(z(1,i) - H*x_k);
+            x(:,i) = x(:,i) + G*(z(1,i) - H*x(:,i));
+			xF(:,i) = xF(:,i) + K*(z(1,i)-H*xF(:,i));
         end
-    end
+    end    
 end
 
-plot(k, z(1,:), 'r')
-title('Kalman filter');
-xlabel('t, c');
-ylabel('coordinate, m');
-hold on;
-plot(k, x(1,:), 'y')
-legend('meassurement', 'filter');
+% subplot(1,3,1);
+% plot(k, x(1,:), '-', k, z(1,:), 'r-.');
+% title('Coordinate Filter');
+% xlabel('t, c');
+% ylabel('coordinate, m');
+% legend('meassurement', 'filter');
+% 
+% subplot(1,3,2);
+% plot(k, x(2,:), '-', k, z(2,:), 'r-.');
+% title('Velocity Filter');
+% xlabel('t, c');
+% ylabel('velocity, m/c');
+% legend('meassurement', 'filter');
+% 
+% subplot(1,3,3);
+% plot(k, x(3,:), '-', k, z(3,:), 'r-.');
+% title('Acceleration Filter');
+% xlabel('t, c');
+% ylabel('acceleration, m/c*c');
+% legend('meassurement', 'filter');
--- Matlab/Kalman_moving.m
+++ Matlab/Kalman_moving.m
@@ -1,9 +1,6 @@
 function dy = Kalman_moving(t,y)
-    dy = zeros(6,1);
+    dy = zeros(3,1);
     dy(3) = 0;
     dy(2) = y(3);
     dy(1) = y(2);
-    dy(6) = 0;
-    dy(5) = y(6);
-    dy(4) = y(5);
 end
